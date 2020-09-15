5feb5850f9da25f9773adf6796edcdce531c3399
LoboArkano <pipiolo_mananes@hotmail.com>
2020-09-15 15:35:59 -0500

    Test for calculator module

---
+++ calculator.js
@@ -0,0 +1,21 @@
+const calculator = (() => {
+  const add = (x, y) => x + y;
+  const sub = (x, y) => x - y;
+  const mult = (x, y) => x * y;
+  const div = (x, y) => {
+    if (y > 0) {
+      return x / y;
+    } else {
+      return NaN;
+    }
+  }
+
+  return {
+    add,
+    sub,
+    mult,
+    div,
+  }
+})();
+
+export default calculator;
