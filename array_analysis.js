e3cb8c7a6f7cc18b7ca0b7881a3e2fc3f8699db1
LoboArkano <pipiolo_mananes@hotmail.com>
2020-09-16 10:32:09 -0500

    Add arrayAnalysis functions
    Add test to check if the array contains a string

--- array_analysis.js
+++ array_analysis.js
@@ -1,4 +1,14 @@
 const ArrayAnalysis = (array) => {
+  if(array.some((elem) => typeof elem === 'string')) {
+    throw new Error('The array contains a string');
+  }
+
+  const average = array.reduce((a, b) => a + b) / array.length;
+  const maximum = Math.max(...array);
+  const minimum = Math.min(...array);
+  const length = array.length;
+
+  return { average, maximum, minimum, length}
 };
 
 export default ArrayAnalysis;
--- array_analysis.test.js
+++ array_analysis.test.js
@@ -1,27 +1,42 @@
 import ArrayAnalysis from './array_analysis';
 
 const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const testArray2 = [1, 2, 3, 's', 5, 6, 7, 8, 'o', 10];
 
 test('Average should be 5.5', () => {
-  expect(ArrayAnalysis(testArray).objectContaining({
-    average: 5.5,
-  }));
+  expect(ArrayAnalysis(testArray)).toEqual(
+    expect.objectContaining({
+      average: 5.5,
+    })
+  );
 });
 
 test('Minimum should be 1', () => {
-  expect(ArrayAnalysis(testArray).objectContaining({
-    minimum: 1,
-  }));
+  expect(ArrayAnalysis(testArray)).toEqual(
+    expect.objectContaining({
+      minimum: 1,
+    })
+  );
 });
 
 test('Maximum should be 10', () => {
-  expect(ArrayAnalysis(testArray).objectContaining({
-    maximum: 10,
-  }));
+  expect(ArrayAnalysis(testArray)).toEqual(
+    expect.objectContaining({
+      maximum: 10,
+    })
+  );
 });
 
 test('Length should be 10', () => {
-  expect(ArrayAnalysis(testArray).objectContaining({
-    length: 10,
-  }));
+  expect(ArrayAnalysis(testArray)).toEqual(
+    expect.objectContaining({
+      length: 10,
+    })
+  );
+});
+
+test('The array have a string', () => {
+  expect(() => {
+    ArrayAnalysis(testArray2)
+  }).toThrow('The array contains a string');
 });
